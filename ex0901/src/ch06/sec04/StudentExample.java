21c10a9f1a9e95d35a72e2252fd99a08ff979968
crist-17 <pci1234good@gmail.com>
2025-09-08 17:39:29 +0900

    Update StudentExample.java

--- ex0901/src/ch06/sec04/StudentExample.java
+++ ex0901/src/ch06/sec04/StudentExample.java
@@ -5,10 +5,10 @@ public class StudentExample {
 	public static void main(String[] args) {
 		
 		Student s1 = new Student();
-		System.out.println("s1 변수가  System 객체를 참조합니다.");
+		System.out.println("s1 변수가  Student 객체를 참조합니다.");
 		
 		Student s2 = new Student();
-		System.out.println("s2 변수가 또 다른 System 객체를 참조합니다.");
+		System.out.println("s2 변수가 또 다른 Student 객체를 참조합니다.");
 		
 		System.out.println(s1);
 		System.out.println(s2);
