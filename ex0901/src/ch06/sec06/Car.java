fcaf315c871465605e0e6eaee1bab68c8449a477
crist-17 <pci1234good@gmail.com>
2025-09-09 02:50:03 +0900

    home add

---
+++ ex0901/src/ch06/sec06/Car.java
@@ -0,0 +1,15 @@
+package ch06.sec06;
+
+public class Car {
+
+	String model = "그랜쳐";
+	String company = "그랜쳐";
+	String color = "그랜쳐";
+	boolean start ;
+	int maxSpeed = 200;
+	int speed;
+	
+
+	}
+
+
